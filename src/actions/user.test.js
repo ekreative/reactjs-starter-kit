f681a461a3f827ed7f41fa6c17a6319a31d91da6
Andrii Petrash <andrii.petrash@gmail.com>
2019-11-14 23:28:43 +0200

    remove tests

--- src/actions/user.test.js
+++
@@ -1,5 +0,0 @@
-/* eslint-env jest */
-describe('login', () => {
-  it('failure response', () => {})
-  it('success response', () => {})
-})
--- src/services/sort.test.js
+++
@@ -1,7 +0,0 @@
-/* eslint-env jest */
-describe('sort array', () => {
-  it('array is sorted', () => {
-  })
-  it('array is not sorted', () => {
-  })
-})
