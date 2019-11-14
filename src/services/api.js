63c7aa6335a8b360fd9b11c8cf258e992837b538
Andrii Petrash <andrii.petrash@gmail.com>
2019-11-14 23:13:06 +0200

    refactor API service

--- src/services/api.js
+++ src/services/api.js
@@ -1,50 +1,50 @@
 import axios from 'axios'
 import {API_HOST} from '../config'
 
-// const api = axios.create({
-//   baseURL: API_HOST,
-//   headers: {
-//     'Content-Type': 'application/json'
-//   }
-// })
-//
-// api.interceptors.request.use(
-//   config => {
-//     return config
-//   },
-//   error => {
-//     Promise.reject(error)
-//   }
-// )
-//
-// api.interceptors.response.use(
-//   response => {
-//     return response
-//   },
-//   error => {
-//     if (error.response) {
-//       // The request was made, but the server responded with a status code
-//       // that falls out of the range of 2xx
-//       console.log(error.response.data)
-//       console.log(error.response.status)
-//       console.log(error.response.headers)
-//     } else {
-//       // Something happened in setting up the request that triggered an Error
-//       console.log('Strange Error', error.message)
-//     }
-//   }
-// )
+const api = axios.create({
+  baseURL: API_HOST,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+api.interceptors.request.use(
+  config => {
+    return config
+  },
+  error => {
+    Promise.reject(error)
+  }
+)
+
+api.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    if (error.response) {
+      // The request was made, but the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data)
+      console.log(error.response.status)
+      console.log(error.response.headers)
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Strange Error', error.message)
+    }
+  }
+)
 
 export default class API {
   static get (id) {
-    return axios.get(`/some/${id}`)
+    return api.get(`/some/${id}`)
       .then(response => {
         return response.data // return response;
       })
   }
 
   static post (data) {
-    return axios.post(`/some`, data)
+    return api.post(`/some`, data)
       .then(response => {
         return response.data // return response;
       })
