40f47e0e2db876e3d9f3c6cb3f6b4d9135321a81
Andrii Petrash <andrii.petrash@gmail.com>
2019-11-14 17:34:28 +0200

    clear tests

--- src/services/sort.test.js
+++ src/services/sort.test.js
@@ -1,14 +1,7 @@
 /* eslint-env jest */
-import sort from './sort'
-
-const inArray = [2, 8, 12, 5]
-const outArray = [2, 5, 8, 12]
-
 describe('sort array', () => {
   it('array is sorted', () => {
-    expect(sort(inArray)).toEqual(outArray)
   })
   it('array is not sorted', () => {
-    expect(sort(inArray)).toEqual(inArray)
   })
 })
--- src/services/validation.test.js
+++ src/services/validation.test.js
@@ -1,30 +1,18 @@
 /* eslint-env jest */
-import { isEmailValid, isPasswordValid } from './validation'
-
 describe('email validation', () => {
   describe('email is valid', () => {
-    it('standart email', () => {expect(isEmailValid('test@test.com')).toBe(true)})
-    it('with numbers email', () => {expect(isEmailValid('1234@test.com')).toBe(true)})
+    it('standart email', () => {})
   })
   describe('email is invalid', () => {
-    it('empty email', () => {expect(isEmailValid('')).toBe(false)})
-    it('without @ email', () => {expect(isEmailValid('test')).toBe(false)})
-    it('number email', () => {expect(isEmailValid(1234)).toBe(false)})
-    it('space in email', () => {expect(isEmailValid('test @ test.com')).toBe(false)})
-    it('without dot email', () => {expect(isEmailValid('test@test')).toBe(false)})
+    it('is not email', () => {})
   })
 })
 
 describe('password validation', () => {
   describe('password is valid', () => {
-    it('ok password', () => {expect(isPasswordValid('Password12345678')).toBe(true)})
-    it('enough password', () => {expect(isPasswordValid('assWord911')).toBe(true)})
+    it('standart password', () => {})
   })
   describe('password is invalid', () => {
-    it('empty password', () => {expect(isPasswordValid('')).toBe(false)})
-    it('short password', () => {expect(isPasswordValid('Pass123')).toBe(false)})
-    it('space in password', () => {expect(isPasswordValid('test Resr 123')).toBe(false)})
-    it('special symbols in password', () => {expect(isPasswordValid('^$^%$^^')).toBe(false)})
+    it('is not email', () => {})
   })
-
 })
