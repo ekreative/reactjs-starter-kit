d1855b66f3bd36a7f29cdebad0fed794e38b9c25
DmytroTrubetskoi <dmytro.trubetskoi@ekreative.com>
2020-01-24 12:52:19 +0200

    added pages for starships

--- src/components/ComponentStarships/ComponentStarships.tsx
+++ src/components/ComponentStarships/ComponentStarships.tsx
@@ -4,28 +4,52 @@ import "./ComponentStarships.css";
 import { IPropsStarships } from "./ComponentStarshipsInterfaces";
 import API from "../../services/api";
 import { IElement } from "../ComponentPerson/ComponentPersonInterfaces";
-import ComponsntSingleStarshipContainer from "../ComponentSingleStarship/ComponsntSingleStarshipContainer"
+import ComponsntSingleStarshipContainer from "../ComponentSingleStarship/ComponsntSingleStarshipContainer";
 
 export const ComponentStarships: React.FC<IPropsStarships> = props => {
   const [starships, setStarships] = useState({ results: [] });
+  const [swStarshipsPage, setSwStarshipsPage] = useState(1);
 
-  const getSWPeople = async () => {
+  const getSWStarships = async () => {
     console.log(starships);
     let peopleData = await API.get(`https://swapi.co/api/starships/?page=1`);
     await setStarships(peopleData);
   };
+
+  const getNextPageSWStarships = async () => {
+    if (swStarshipsPage < 4) {
+      setSwStarshipsPage(swStarshipsPage + 1);
+      let starshipsData = await API.get(
+        `https://swapi.co/api/starships/?page=${swStarshipsPage + 1}`
+      );
+      await setStarships(starshipsData);
+    }
+  };
+
+  const getPrevPageSWStarships = async () => {
+    if (swStarshipsPage > 1) {
+      setSwStarshipsPage(swStarshipsPage - 1);
+      let starshipsData = await API.get(
+        `https://swapi.co/api/starships/?page=${swStarshipsPage - 1}`
+      );
+      await setStarships(starshipsData);
+    }
+  };
   return (
-    <>
-      <button className="ComponentA" onClick={getSWPeople}>getStarships</button>
+    <div className={"StarshipsData"}>
+      <button onClick={getPrevPageSWStarships}> {`<<<`} </button>
+      <button onClick={getSWStarships}>getStarships</button>
+      <button onClick={getNextPageSWStarships}> {`>>>`} </button>
       {starships?.results.map((element: IElement, id: number) => {
         return (
           <div>
-            <ComponsntSingleStarshipContainer key={id} element={element}/>
-            <div>123</div>
+            <div>
+              <ComponsntSingleStarshipContainer key={id} element={element} />
+            </div>
           </div>
         );
       })}
-    </>
-
+      <div>some Text</div>
+    </div>
   );
 };
