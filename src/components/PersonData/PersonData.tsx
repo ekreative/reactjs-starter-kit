62f24597205521105c24f1908add9f36d0049704
DmytroTrubetskoi <dmytro.trubetskoi@ekreative.com>
2020-01-24 16:16:40 +0200

    created styled components for PersonData.tsx with example inside

--- src/components/PersonData/PersonData.tsx
+++ src/components/PersonData/PersonData.tsx
@@ -1,12 +1,11 @@
 // @ts-ignore
 import React from "react";
-import "./PersonData.scss";
 import { IPropsData } from "./PersonDataInterfaces";
 
 export const PersonData: React.FC<IPropsData> = props => {
   return (
-    <div className="ComponentA">
-      <div className="PersonName" onClick={()=>{props.getOnePersonData(props.element.url)}}>
+    <div>
+      <div onClick={()=>{props.getOnePersonData(props.element.url)}}>
         <p>{props.swPeoplePage * 10 + props.id + 1 - 10}. {props.element.name}</p>
       </div>
     </div>
---
+++ src/components/PersonData/PersonDataElements.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+//example of styled-component
+//export const PersonContainer = styled.div`
+//   display: flex;
+//   justify-content: space-around;
+// `;
