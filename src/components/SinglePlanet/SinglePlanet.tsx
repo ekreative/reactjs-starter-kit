414ef34ad3385336a6681e3ee90c153e31b9cb57
DmytroTrubetskoi <dmytro.trubetskoi@ekreative.com>
2020-02-03 10:33:01 +0200

    added links to planets

--- src/components/SinglePlanet/SinglePlanet.tsx
+++ src/components/SinglePlanet/SinglePlanet.tsx
@@ -1,6 +1,6 @@
-// @ts-ignore
 import React from "react";
 import { SinglePlanetsElement } from "./SinglePlanetElements";
+import { Link } from "react-router-dom";
 
 interface IPropsSinglePlanet {
   id: number;
@@ -14,13 +14,16 @@ interface IPropsSinglePlanet {
 }
 
 export const SinglePlanet: React.FC<IPropsSinglePlanet> = props => {
+
   return (
     <SinglePlanetsElement
       onClick={() => {
         props.getSinglePlanetData(props.element.url);
       }}
     >
+      <Link to={`/planet/${props.element.name}`}>
       {props.swPlanetsPage * 10 + props.id + 1 - 10}. {props.element.name}
+      </Link>
     </SinglePlanetsElement>
   );
 };
