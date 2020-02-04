0cd11a9ec4f1da54d0c9482ee9196302adac24a8
DmytroTrubetskoi <dmytro.trubetskoi@ekreative.com>
2020-02-04 17:43:07 +0200

    added tooltips to GoogleMapsMark.tsx,

--- src/components/GoogleMapsMark/GoogleMapsMark.tsx
+++ src/components/GoogleMapsMark/GoogleMapsMark.tsx
@@ -1,14 +1,24 @@
 import React from "react";
-import {} from './GoogleMapsMarkElements'
+import Popup from "reactjs-popup";
 
-interface IGoogleMapsMark {
-  key: number;
-  lat: number;
-  lng: number;
-  text: string;
-}
-const GoogleMapsMark: React.FC<IGoogleMapsMark> = (props) => {
-  return (<div>{props.text}</div>);
-};
+const Card = ({ title }: any) => (
+  <div className="card">
+    <div className="header">{title} position </div>
+    <div className="content">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit autem
+      sapiente labore architecto exercitationem optio quod dolor cupiditate
+    </div>
+  </div>
+);
+
+const GoogleMapsMark = () => (
+  <Popup
+    trigger={<button className="button"> Bottom Center </button>}
+    position="bottom center"
+    on="hover"
+  >
+    <Card title="Bottom Center" />
+  </Popup>
+);
 
 export default GoogleMapsMark;
